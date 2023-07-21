899fd9076b8f3663a182d6cf340ae67c2f11ff97
roganOhDev <fdscbjdcnhd@gmail.com>
2023-07-21 16:23:24 -0700

    download file from s3

---
+++ download.js
@@ -0,0 +1,4 @@
+import { download } from "./s3Core.js";
+
+let res;
+download('franfe-raw-cafe-images', 'querypie.jpeg', './querypie.jpeg').then(r =>{});
--- s3Core.js
+++ s3Core.js
@@ -2,6 +2,7 @@ import { awsAccessKeyId, awsSecretAccessKey } from "./config.js";
 import AWS from 'aws-sdk';
 
 import fs from "fs";
+
 const region = 'us-west-1'
 
 export const upload = async (bucketName, file, filePath) => {
@@ -28,3 +29,35 @@ export const upload = async (bucketName, file, filePath) => {
     }
   });
 };
+
+
+export const download = async (bucketName, file, path) => {
+
+  AWS.config.update({
+    accessKeyId: awsAccessKeyId,
+    secretAccessKey: awsSecretAccessKey,
+    region: region,
+  });
+
+  const s3 = new AWS.S3();
+
+  const options = {
+    Bucket: bucketName,
+    Key: file,
+  };
+
+  try {
+
+    const fileStream = s3.getObject(options).createReadStream();
+    fileStream.pipe(fs.createWriteStream(path))
+      .on('error', function (err) {
+        console.error('Error saving file:', err);
+      })
+      .on('close', function () {
+        console.log('File saved successfully.');
+      });
+
+  } catch (err) {
+    console.error(err);
+  }
+}
